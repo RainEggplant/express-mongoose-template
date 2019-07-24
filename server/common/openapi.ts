4b115e2c5d4d1d03c9e8a3712c9d544210c70779
RainEggplant <i@raineggplant.com>
2019-07-24 18:32:10 +0800

    Add type annotations

--- server/common/openapi.ts
+++ server/common/openapi.ts
@@ -1,9 +1,13 @@
 import path from "path";
 import express from "express";
+import { Application } from "express";
 import { OpenApiValidator } from "express-openapi-validator";
 import errorHandler from "../api/middlewares/error.handler";
 
-export default function openapi(app, routes) {
+export default function openapi(
+  app: Application,
+  routes: (app: Application) => void
+) {
   const apiSpecPath = path.join(__dirname, "api.yml");
   app.use(process.env.OPENAPI_SPEC || "/spec", express.static(apiSpecPath));
 
